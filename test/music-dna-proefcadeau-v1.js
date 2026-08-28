1d77560eb6f3febd8a63fce6e5583d1bdacd3020
bcim1958 <bcim@zeelandnet.nl>
2026-08-28 11:34:13 +0200

    Add isolated 21-track Music DNA proefcadeau

---
+++ test/music-dna-proefcadeau-v1.js
@@ -0,0 +1,29 @@
+const MUSIC_DNA_PROEFCADEAU_V1={
+  id:"proefcadeau-2026-08-28",
+  label:"Proefcadeau",
+  purpose:"Isolated test of Saturday gift presentation and Spotify delivery; not learning data and not a real weekly harvest.",
+  tracks:[
+    {artist:"Aerosmith",title:"Toys In The Attic",year:1975,country:"Verenigde Staten",route:"Hard Rock",spotifyUri:"spotify:track:4pj8ElgSWmmy5LT9a7jb0s"},
+    {artist:"The Babys",title:"Wrong or Right",year:1977,country:"Verenigd Koninkrijk",route:"Hard Rock",spotifyUri:"spotify:track:1dCLRc0zCX9My6LuWU7UEy"},
+    {artist:"Giuffria",title:"Do Me Right",year:1984,country:"Verenigde Staten",route:"Hard Rock",spotifyUri:"spotify:track:1IsKwftNuXKGG9bjNXh7lU"},
+    {artist:"Stiff Little Fingers",title:"Alternative Ulster",year:1979,country:"Verenigd Koninkrijk",route:"Punk/New Wave",spotifyUri:"spotify:track:2yE3bwbhqypdsuhmv48Svn"},
+    {artist:"Ideal",title:"Eiszeit",year:1981,country:"Duitsland",route:"Punk/New Wave",spotifyUri:"spotify:track:4WOGvfip17yNBmLpPSZWBV"},
+    {artist:"Orchestral Manoeuvres In The Dark",title:"Electricity - Remastered 2003",year:1980,country:"Verenigd Koninkrijk",route:"Punk/New Wave",spotifyUri:"spotify:track:60TmG8cFPMBo7kTTFOLiAB"},
+    {artist:"The Runaways",title:"Wasted",year:1977,country:"Verenigde Staten",route:"Alternative/Indie Rock",spotifyUri:"spotify:track:6PXUO3U78bz7ygA2jirvOE"},
+    {artist:"Big Audio Dynamite",title:"E=MC2",year:1985,country:"Verenigd Koninkrijk",route:"Alternative/Indie Rock",spotifyUri:"spotify:track:52WaKEZpbiD6aBkmENlj6x"},
+    {artist:"The Donnas",title:"Take It Off",year:2003,country:"Verenigde Staten",route:"Alternative/Indie Rock",spotifyUri:"spotify:track:6QyBIZEvs11K9lKjyLYtv6"},
+    {artist:"Metal Church",title:"Watch the Children Pray",year:1986,country:"Verenigde Staten",route:"Heavy Metal",spotifyUri:"spotify:track:2BNvvoD2p6dHWSDDNM9jml"},
+    {artist:"Lacuna Coil",title:"Senzafine",year:2005,country:"Italië",route:"Heavy Metal",spotifyUri:"spotify:track:0WZXtSE1w08YDvU0nA7nF7"},
+    {artist:"UnSun",title:"Whispers",year:2008,country:"Polen",route:"Heavy Metal",spotifyUri:"spotify:track:5AYQhD8erZU6ki7D9haQNB"},
+    {artist:"The Prodigy",title:"Firestarter",year:1997,country:"Verenigd Koninkrijk",route:"Electronic",spotifyUri:"spotify:track:1GTPxha6U7x9ElVxkQw3OK"},
+    {artist:"Hooverphonic",title:"Mad About You",year:2000,country:"België",route:"Electronic",spotifyUri:"spotify:track:5drK2kTE2mrUdV33iHWyrx"},
+    {artist:"Apollo 440",title:"Stop the Rock",year:1998,country:"Verenigd Koninkrijk",route:"Electronic",spotifyUri:"spotify:track:37jrubWAb4F9TY2GgSRG56"},
+    {artist:"Alquin",title:"Wheelchair Groupie",year:1975,country:"Nederland",route:"Art-Rock/Experimental",spotifyUri:"spotify:track:0hKFfPDS68fte1pVaxRiqB"},
+    {artist:"Earth & Fire",title:"Maybe Tomorrow, Maybe Tonight",year:1973,country:"Nederland",route:"Art-Rock/Experimental",spotifyUri:"spotify:track:4QGkmpL7J862ZYwCusgxie"},
+    {artist:"Ange",title:"Ces Gens-Là",year:1989,country:"Frankrijk",route:"Art-Rock/Experimental",spotifyUri:"spotify:track:41oq0POgWxEek6JeeEZO7n"},
+    {artist:"BABYMETAL",title:"Megitsune",year:2015,country:"Japan",route:"Pop/Rock",spotifyUri:"spotify:track:5n8CoyFNnQ7bvj5GgEetzz"},
+    {artist:"BUCK-TICK",title:"JUST ONE MORE KISS",year:1989,country:"Japan",route:"Pop/Rock",spotifyUri:"spotify:track:7BizHmvHLGE4rD2bi7HFDl"},
+    {artist:"Blood Command",title:"A Villain's Monologue",year:2021,country:"Noorwegen",route:"Pop/Rock",spotifyUri:"spotify:track:1Kp80mQhmtLSJCJwzLrnXP"}
+  ],
+  rules:{learning:false,weeklyHarvest:false,positiveBank:false,removeFromFutureDiscovery:false}
+};
