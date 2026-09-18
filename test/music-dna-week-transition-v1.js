ab70851a4a61067e7c8365ba536dd9cae7589ffa
bcim1958 <bcim@zeelandnet.nl>
2026-09-18 11:10:12 +0200

    Add non-destructive week continuity diagnostics

--- test/music-dna-week-transition-v1.js
+++ test/music-dna-week-transition-v1.js
@@ -1,5 +1,5 @@
 (function(){
-  var VERSION=1;
+  var VERSION=2;
   var RATING_WEIGHT={raak:2,goed:1,twijfel:0,nee:-2};
 
   function read(key,fallback){try{var x=window.localStorage.getItem(key);return x?JSON.parse(x):fallback;}catch(e){return fallback;}}
@@ -33,6 +33,25 @@
     return Object.keys(model.signals||{});
   }
 
+
+  function reserveSelectionKey(week,d){return 'bmd-week-'+week+'-day'+d+'-reserve-selection-v1';}
+  function reserveStateKey(week,d){return 'bmd-week-'+week+'-day'+d+'-reserve-v1';}
+  function weekSnapshot(week){
+    var official=weekSignals(week),reserveOffered=0,reserveRated=0,reservePositive=0;
+    for(var d=1;d<=7;d++){
+      var sel=read(reserveSelectionKey(week,d),null),ids=sel?(sel.ids||(sel.id?[sel.id]:[])):[],state=read(reserveStateKey(week,d),{});
+      reserveOffered+=ids.length;
+      for(var i=0;i<ids.length;i++){var r=state[ids[i]]&&state[ids[i]].rating;if(r){reserveRated++;if(r==='raak'||r==='goed')reservePositive++;}}
+    }
+    return {week:week,official:official,officialSummary:ratingSummary(official.signals),reserve:{offered:reserveOffered,rated:reserveRated,positive:reservePositive}};
+  }
+  function continuity(fromWeek,toWeek){
+    var from=weekSnapshot(fromWeek),to=weekSnapshot(toWeek),learning=read('bmd-learning-v1',{signals:{}}),bank=read('bmd-positive-bank-v1',{items:{}}),history=read('bmd-positive-playlist-history-v1',{weeks:{}}),gift=read('bmd-gift-'+fromWeek+'-v1',null),delivery=read('bmd-delivery-'+fromWeek+'-v1',null);
+    var toOfficial=to.official.signals||[],toIds={};for(var i=0;i<toOfficial.length;i++)toIds[toOfficial[i].trackId]=1;
+    var preservedLearning=Object.keys(learning.signals||{}).filter(function(id){return !toIds[id];}).length;
+    return {version:VERSION,fromWeek:fromWeek,toWeek:toWeek,fromComplete:!!from.official.complete,toStarted:toOfficial.length>0,from:from,to:to,learningSignalCount:Object.keys(learning.signals||{}).length,preservedPriorLearningCount:preservedLearning,positiveBankCount:Object.keys(bank.items||{}).length,historyHasFromWeek:!!(history.weeks&&history.weeks[fromWeek]),giftStored:!!gift,deliveryStored:!!delivery,checkedAt:new Date().toISOString()};
+  }
+
   function candidatePlan(fromWeek,toWeek,candidates,options){
     options=options||{};
     var target=Math.max(24,Number(options.target)||30),source=weekSignals(fromWeek);
@@ -61,5 +80,5 @@
     };
   }
 
-  window.MUSIC_DNA_WEEK_TRANSITION={version:VERSION,weekSignals:weekSignals,ratingSummary:ratingSummary,candidatePlan:candidatePlan};
+  window.MUSIC_DNA_WEEK_TRANSITION={version:VERSION,weekSignals:weekSignals,ratingSummary:ratingSummary,weekSnapshot:weekSnapshot,continuity:continuity,candidatePlan:candidatePlan};
 })();
