37f4cc72a9b7b47304d2a5da199770e329272289
bcim1958 <bcim@zeelandnet.nl>
2026-09-21 18:51:37 +0200

    Add Explorer story source registry

---
+++ test/music-dna-explorer-story-sources-v1.js
@@ -0,0 +1,44 @@
+window.MUSIC_DNA_EXPLORER_STORY_SOURCES_V1={
+  version:1,
+  stories:{
+    "maiden-story":{
+      label:"Ghost + Iron Maiden",
+      sources:[{
+        kind:"interview",
+        title:"Kiss the Go-Ghost: An Interview with Tobias Forge",
+        meta:"SLUG Magazine · Zaina Abujebarah · 18 oktober 2019",
+        supports:"Onderbouwt Forge’s jeugdige studie van Iron Maiden-tourroutes en zijn latere reflectie op het leren van Iron Maiden en Metallica tijdens gezamenlijke tournees.",
+        links:[{label:"Originele publicatie · SLUG Magazine",url:"https://www.slugmag.com/music/interviews/kiss-the-go-ghost-an-interview-with-tobias-forge/"}]
+      }]
+    },
+    "metallica-story":{
+      label:"Ghost + Metallica",
+      sources:[
+        {
+          kind:"interview",
+          title:"“Metallica were always there”: the story behind Ghost’s Enter Sandman cover",
+          meta:"Metal Hammer / Louder · Dave Everley · 20 september 2022",
+          supports:"Onderbouwt Forge’s jeugdrelatie met Metallica, de Polar Music Prize-uitvoering en de latere Ghost-opname van Enter Sandman voor The Metallica Blacklist.",
+          links:[{label:"Originele publicatie · Louder",url:"https://www.loudersound.com/features/tobias-forge-metallica-enter-sandman"}]
+        },
+        {
+          kind:"interview",
+          title:"Kiss the Go-Ghost: An Interview with Tobias Forge",
+          meta:"SLUG Magazine · Zaina Abujebarah · 18 oktober 2019",
+          supports:"Onderbouwt Forge’s beschrijving van Iron Maiden en Metallica als professionele leerscholen en bevestigt de context van Ghost’s tournees met beide bands.",
+          links:[{label:"Originele publicatie · SLUG Magazine",url:"https://www.slugmag.com/music/interviews/kiss-the-go-ghost-an-interview-with-tobias-forge/"}]
+        }
+      ]
+    },
+    "singer-story":{
+      label:"Wie had Ghost bijna gezongen?",
+      sources:[{
+        kind:"interview-samenvatting",
+        title:"Ghost: 10 Things We Learned From Our Revealing Interview With Tobias Forge",
+        meta:"Revolver · Revolver Staff · 1 juni 2018",
+        supports:"Onderbouwt dat Forge aanvankelijk niet de zanger wilde zijn en dat Messiah Marcolin, Mats Levén, Christer Göransson en JB Christoffersson de zangrol aangeboden kregen maar niet instapten.",
+        links:[{label:"Originele publicatie · Revolver",url:"https://www.revolvermag.com/music/ghost-10-things-we-learned-our-revealing-interview-tobias-forge/"}]
+      }]
+    }
+  }
+};
